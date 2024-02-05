deb1242058bd09ce9ba0bb34c29bb0ce3f980275
PiPiPAPADA <shijiepppapa@pku.org.cn>
2024-02-05 16:53:44 +0800

    ADD keyboard

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,13 @@
 #include "../drives/screen.h"
-#include "util.h"
 #include "../cpu/isr.h"
-#include "../cpu/idt.h"
+#include "../cpu/timer.h"
+#include "../drives/keyboard.h"
 
 void main(){
     isr_install();
     /* Test the interrupts */
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
+     asm volatile("sti");
+    // init_timer(50);
+    init_keyboard();
+
 }
--- kernel/util.c
+++ kernel/util.c
@@ -23,6 +23,25 @@ void int_to_ascii(int n, char str[]) {
 
     if (sign < 0) str[i++] = '-';
     str[i] = '\0';
+    reverse(str);
 
     /* TODO: implement "reverse" */
+}
+
+int strlen(char s[]){
+    int i =0;
+    while (s[i]!='\0')
+    {
+        ++i;
+    }
+    return i;    
+}
+
+void reverse(char s[]){
+    int c,i,j;
+    for(i=0,j=strlen(s)-1;i<j;i++,j--){
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
