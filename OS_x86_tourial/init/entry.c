e3774c783657ecaf81a6b8a6bdbb043d0490b355
PiPiPAPADA <shijiepppapa@pku.org.cn>
2024-02-08 15:06:56 +0800

    ADD GDT

--- OS_x86_tourial/init/entry.c
+++ OS_x86_tourial/init/entry.c
@@ -1,11 +1,13 @@
 #include "console.h"
 #include "debug.h"
+#include "gdt.h"
 
 int kernel_entry(){
     init_debug();
+    init_gdt();
     console_clear();
     printk_color(rc_black,rc_cyan,"HALO, Os Kernel\n");
-    panic("test");
+    // panic("test");
     // console_write_color("HALO,OS kernel!\n",rc_black,rc_green);
     return 0;
 }
