18087af5370f413bc9b7eeae5ca4a49aacb4a7af
PiPiPAPADA <shijiepppapa@pku.org.cn>
2024-02-08 15:32:12 +0800

    ADD IDT

--- OS_x86_tourial/init/entry.c
+++ OS_x86_tourial/init/entry.c
@@ -5,8 +5,11 @@
 int kernel_entry(){
     init_debug();
     init_gdt();
+    init_idt();
     console_clear();
     printk_color(rc_black,rc_cyan,"HALO, Os Kernel\n");
+    asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
     // panic("test");
     // console_write_color("HALO,OS kernel!\n",rc_black,rc_green);
     return 0;
