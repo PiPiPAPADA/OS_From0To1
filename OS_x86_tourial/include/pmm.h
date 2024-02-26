0e1b6b54667cc6c6f7a1a6639713e029b1533d21
PiPiPAPADA <shijiepppapa@pku.org.cn>
2024-02-26 14:10:39 +0800

    ADD page memory manager

--- OS_x86_tourial/include/pmm.h
+++ OS_x86_tourial/include/pmm.h
@@ -6,8 +6,25 @@
 //内核文件的起始结束地址，在链接器中要求链接器重新定义
 extern uint8_t kern_start[];
 extern uint8_t kern_end[];
+extern uint32_t phy_page_count;
 
 //输出BIOS提供的物理内存布局
 void show_memory_map();
+void init_pmm();
+uint32_t pmm_alloc_page();
+void pmm_free_page(uint32_t p);
+
+//thread stack size
+#define STACK_SIZE 8192
+
+//max phy memory (512MB)
+#define PMM_MAX_SIZE 0x20000000
+//phy memory size 
+#define PMM_PAGE_SIZE 0x1000
+//phy page num
+#define PAGE_MAX_SIZE (PMM_MAX_SIZE/PMM_PAGE_SIZE)
+//page mask 4096 align
+#define PHY_PAGE_MASK 0xFFFFF000
+
 
 #endif
--- OS_x86_tourial/init/entry.c
+++ OS_x86_tourial/init/entry.c
@@ -18,6 +18,21 @@ int kernel_entry(){
     printk("kernel in memory end: 0x%08X\n",kern_end);
     printk("kernel in memory used: %d KB \n\n",(kern_end-kern_start+1023)/1024);
     show_memory_map();
+    init_pmm();
+
+    uint32_t  alloc_addr = NULL;
+    printk_color(rc_black,rc_light_brown,"Test Phy memory alloc:\n");
+    alloc_addr = pmm_alloc_page();
+    printk_color(rc_black,rc_light_brown,"Alloc Phy memory addr: 0x%08X\n",alloc_addr);
+    alloc_addr = pmm_alloc_page();
+    printk_color(rc_black,rc_light_brown,"Alloc Phy memory addr: 0x%08X\n",alloc_addr);
+    alloc_addr = pmm_alloc_page();
+    printk_color(rc_black,rc_light_brown,"Alloc Phy memory addr: 0x%08X\n",alloc_addr);
+    alloc_addr = pmm_alloc_page();
+    printk_color(rc_black,rc_light_brown,"Alloc Phy memory addr: 0x%08X\n",alloc_addr);
+    pmm_free_page(alloc_addr);
+    alloc_addr = pmm_alloc_page();
+    printk_color(rc_black,rc_light_brown,"Alloc Phy memory addr: 0x%08X\n",alloc_addr);
     // panic("test");
     // console_write_color("HALO,OS kernel!\n",rc_black,rc_green);
     return 0;
